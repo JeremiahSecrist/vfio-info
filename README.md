559b1413123416f51f62418d5597d3c482c006e3
creatorofthesky <nana@nananana.com>
2020-10-12 19:40:27 -0400

    Initial upload of main.sh

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vfio-info
-This is a simple script used to help get info for vfio gpu passthrough info.
+This is a simple script used to help get info for vfio gpu passthrough info. 
+# Usage
+Its Main usage is for the vfio discord that helps people with issues reguard gpu passthrough for virtual machines. 
+It does this by gathering all the information reguarding your systems configuration and uploading it to clbin and returning you the link to upload in chat
+Feel Free to look over the script before running it as I personaly recomend.
+# How to:
+Preferably you would want to just download and run it in the terminal, 
+but as this is a help tool dealing with individuals of varying skill sets I have also linked below a simple way to run it.
+
+Simply copy and paste the following into your terminal and type in your password:
+	curl -L https://raw.githubusercontent.com/creatorofthesky/vfio-info/master/main.sh | bash 
---
+++ main.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+while true
+do
+  sudo virsh list --all
+  read -p "Please type in the VM name exactly: " answer
+    if sudo virsh list --all --name | grep "$answer";
+        then
+            clear
+            echo -e "\`\`\`\n$answer XML dump:"
+            sudo virsh dumpxml "$answer" | curl -F 'clbin=<-' https://clbin.com
+            sleep 1
+            echo "libvirt status:"
+            systemctl status libvirtd | curl -F 'clbin=<-' https://clbin.com
+            sleep 1
+            echo "Libvirt logs:"
+            journalctl -b -u libvirtd | curl -F 'clbin=<-' https://clbin.com
+            sleep 1
+            echo "qemu.conf:"
+            grep --invert-match -e "^# " -e "^ "- /etc/libvirt/qemu.conf | grep -e "[a-z]" | curl -F 'clbin=<-' https://clbin.com
+            sleep 1
+            echo "Libvirt $answer logs:"
+            cat /var/log/libvirt/qemu/"$answer".log | curl -F 'clbin=<-' https://clbin.com
+            echo "\`\`\`"
+            exit
+    elif [ "$answer" == "exit" ];
+        then
+            echo "Goodbye!"
+            break
+    elif true;
+        then
+            echo "ERROR Incorrect VM name!"
+    fi    
+
+done
