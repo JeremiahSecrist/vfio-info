e75521c0d6663c3222bde9b2c26e1fa4e531f8c2
Trent Michael Arcuri <trent.arcuri@gmail.com>
2020-10-12 20:49:07 -0500

    Remove useless grep and pattern match

--- main.sh
+++ main.sh
@@ -2,35 +2,25 @@
 
 export LIBVIRT_DEFAULT_URI='qemu:///system'
 
-while true
-do
-  virsh list --all
-  read -rp "Please type in the VM name exactly: " answer
-  if sudo virsh list --all --name | grep -q "$answer"
-  then
-    clear
-    echo -e "\`\`\`\n$answer XML dump:"
-    sudo virsh dumpxml "$answer" | curl -F 'clbin=<-' https://clbin.com
-    sleep 1
-    echo "libvirt status:"
-    systemctl status libvirtd | curl -F 'clbin=<-' https://clbin.com
-    sleep 1
-    echo "Libvirt logs:"
-    journalctl -b -u libvirtd | curl -F 'clbin=<-' https://clbin.com
-    sleep 1
-    echo "qemu.conf:"
-    grep --invert-match -e "^# " -e "^ "- /etc/libvirt/qemu.conf | grep -e "[a-z]" | curl -F 'clbin=<-' https://clbin.com
-    sleep 1
-    echo "Libvirt $answer logs:"
-    curl -F 'clbin=<-' https://clbin.com < "/var/log/libvirt/qemu/${answer}".log
-    echo "\`\`\`"
-    exit
-  elif [ "$answer" == "exit" ];
-  then
-    echo "Goodbye!"
-    break
-  elif true;
-  then
-    echo "ERROR Incorrect VM name!"
-  fi
-done
+virsh list --all
+read -rp "Please type in the VM name exactly: " answer
+if sudo virsh domstate "${answer}"
+then
+  clear
+  echo -e "\`\`\`\n$answer XML dump:"
+  sudo virsh dumpxml "$answer" | curl -F 'clbin=<-' https://clbin.com
+  sleep 1
+  echo "libvirt status:"
+  systemctl status libvirtd | curl -F 'clbin=<-' https://clbin.com
+  sleep 1
+  echo "Libvirt logs:"
+  journalctl -b -u libvirtd | curl -F 'clbin=<-' https://clbin.com
+  sleep 1
+  echo "qemu.conf:"
+  grep --invert-match -e "^# " -e "^ "- /etc/libvirt/qemu.conf | grep -e "[a-z]" | curl -F 'clbin=<-' https://clbin.com
+  sleep 1
+  echo "Libvirt $answer logs:"
+  curl -F 'clbin=<-' https://clbin.com < "/var/log/libvirt/qemu/${answer}".log
+  echo "\`\`\`"
+  exit
+fi
