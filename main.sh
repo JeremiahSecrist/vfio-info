0b49ce2c2b44f2675453e2d46b847b91a7d59ab2
creatorofthesky <nana@nananana.com>
2020-10-12 22:47:40 -0400

    Removed the loop and changed the variable name in main.sh

--- main.sh
+++ main.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-while true
-do
-    sudo virsh list --all
-    pause 2
-    read -p "Please type in the VM name exactly: " answer
-    if sudo virsh list --all --name | grep -q "$answer"
-        then
-            clear
-            echo -e "\`\`\`\n$answer XML dump:"
-            sudo virsh dumpxml "$answer" | curl -F 'clbin=<-' https://clbin.com
-            sleep 1
-            echo "libvirt status:"
-            systemctl status libvirtd | curl -F 'clbin=<-' https://clbin.com
-            sleep 1
-            echo "Libvirt logs:"
-            journalctl -b -u libvirtd | curl -F 'clbin=<-' https://clbin.com
-            sleep 1
-            echo "qemu.conf:"
-            grep --invert-match -e "^# " -e "^ "- /etc/libvirt/qemu.conf | grep -e "[a-z]" | curl -F 'clbin=<-' https://clbin.com
-            sleep 1
-            echo "Libvirt $answer logs:"
-            cat /var/log/libvirt/qemu/"$answer".log | curl -F 'clbin=<-' https://clbin.com
-            echo "\`\`\`"
-            exit
-    elif [ "$answer" == "exit" ];
-        then
-            echo "Goodbye!"
-            break
-    elif true;
-        then
-            echo "ERROR Incorrect VM name!"
-    fi    
 
-done
+# Set the default Libvirt URI
+export LIBVIRT_DEFAULT_URI='qemu:///system'
+
+if ! groups | grep -qe libvirt; then
+    echo "You're not in the libvirt group, you may be asked for your sudo password."
+    sudo=sudo
+fi
+    $sudo virsh list --all
+    read -p "Please type in the VM name exactly: " VM
+if $sudo virsh list --all --name | grep -q "${VM}"
+    then
+        clear
+        echo "${VM} XML dump:"
+        $sudo virsh dumpxml "${VM}" | curl -F 'clbin=<-' https://clbin.com
+        sleep 0.5
+        echo "libvirt status:"
+        systemctl status libvirtd | curl -F 'clbin=<-' https://clbin.com
+        sleep 0.5
+        echo "Libvirt logs:"
+        journalctl -b -u libvirtd | curl -F 'clbin=<-' https://clbin.com
+        sleep 0.5
+        echo "qemu.conf:"
+        grep --invert-match -e "^# " -e "^ "- /etc/libvirt/qemu.conf | grep -e "[a-z]" | curl -F 'clbin=<-' https://clbin.com
+        sleep 0.5
+        echo "Libvirt ${VM} logs:"
+        cat /var/log/libvirt/qemu/"${VM}".log | curl -F 'clbin=<-' https://clbin.com
+        exit
+else
+    echo "ERROR Incorrect VM name!"
+fi    
