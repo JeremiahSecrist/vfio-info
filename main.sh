247b8da2926b7962749d4e0e24ce5a8f88f68e5f
Trent Michael Arcuri <trent.arcuri@gmail.com>
2020-10-12 20:50:16 -0500

    Rename answer variable to something more descriptive

--- main.sh
+++ main.sh
@@ -3,12 +3,12 @@
 export LIBVIRT_DEFAULT_URI='qemu:///system'
 
 virsh list --all
-read -rp "Please type in the VM name exactly: " answer
-if sudo virsh domstate "${answer}"
+read -rp "Please type in the VM name exactly: " domain
+if sudo virsh domstate "${domain}"
 then
   clear
-  echo -e "\`\`\`\n$answer XML dump:"
-  sudo virsh dumpxml "$answer" | curl -F 'clbin=<-' https://clbin.com
+  echo -e "\`\`\`\n${domain} XML dump:"
+  sudo virsh dumpxml "${domain}" | curl -F 'clbin=<-' https://clbin.com
   sleep 1
   echo "libvirt status:"
   systemctl status libvirtd | curl -F 'clbin=<-' https://clbin.com
@@ -19,8 +19,8 @@ then
   echo "qemu.conf:"
   grep --invert-match -e "^# " -e "^ "- /etc/libvirt/qemu.conf | grep -e "[a-z]" | curl -F 'clbin=<-' https://clbin.com
   sleep 1
-  echo "Libvirt $answer logs:"
-  curl -F 'clbin=<-' https://clbin.com < "/var/log/libvirt/qemu/${answer}".log
+  echo "Libvirt ${domain} logs:"
+  curl -F 'clbin=<-' https://clbin.com < "/var/log/libvirt/qemu/${domain}".log
   echo "\`\`\`"
   exit
 fi
