187adb02d20bd38ba1fda6efa9c824617f867ede
Sebastiaan <751205+SharkWipf@users.noreply.github.com>
2020-10-13 03:34:21 +0200

    Don't use sudo unless necessary

--- main.sh
+++ main.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+
+# Set the default Libvirt URI
+export LIBVIRT_DEFAULT_URI='qemu:///system'
+
 while true
 do
-    sudo virsh list --all
-    pause 2
+    if ! groups | grep -qe libvirt; then
+        echo "You're not in the libvirt group, you may be asked for your sudo password."
+        sudo=sudo
+    fi
+    $sudo virsh list --all
+    sleep 2
     read -p "Please type in the VM name exactly: " answer
-    if sudo virsh list --all --name | grep -q "$answer"
+    if $sudo virsh list --all --name | grep -q "$answer"
         then
             clear
             echo -e "\`\`\`\n$answer XML dump:"
-            sudo virsh dumpxml "$answer" | curl -F 'clbin=<-' https://clbin.com
+            $sudo virsh dumpxml "$answer" | curl -F 'clbin=<-' https://clbin.com
             sleep 1
             echo "libvirt status:"
             systemctl status libvirtd | curl -F 'clbin=<-' https://clbin.com
